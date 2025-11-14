5069e5512da1347ad4df1baf7e9fc881e0916ee1
Razqa02 <blsmk.250127070@budiluhur.sch.id>
2025-11-14 13:46:46 +0700

    Update script.js

--- script.js
+++ script.js
@@ -20,14 +20,14 @@ if(form){
 }
 
 const scrollTopBtn = document.getElementById("scrollTopBtn");
-window.onscroll =  () => {
-    if (document.documentElement.scrollTop > 200) {
-        scrollTopBtn.style.display = "block" ;
-    } else {
-        scrollTopBtn.style.display = "none";
-    }
-}
+window.onscroll = () => {
+  if (document.documentElement.scrollTop > 200) {
+    scrollTopBtn.style.display = "block";
+  } else {
+    scrollTopBtn.style.display = "none";
+  }
+};
 scrollTopBtn.addEventListener("click", () => {
-    window.scrollTo({top :0, behavior: "smooth"});
+  window.scrollTo({ top: 0, behavior: "smooth" });
 });
 
